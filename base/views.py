d188baaf9cefc269130f4d21c6ee655f03ed1ff9
saurabhsrivastava2001 <srisaurabh2001@gmail.com>
2024-10-24 12:15:53 +0530

    commiting my rest projects i made

--- base/views.py
+++ base/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 rooms=[
-    {'id':1,'name':'learning phase of python django'}   
+    {'id':1,'name':'learning phase of python django'},
+    {'id':2,'name':'not in one day '},
+    {'id':3,'name':'but one day for sure'},
 ]
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{'rooms':rooms})
 
 def room(request):
     return render(request,'room.html')
